5b52feb9c89f4297a93953785ed527a59c6f58bb
ivansepulveda <Physics1738!>
2020-04-06 00:14:52 -0700

    FINALLY optionality loading jsons

--- dynamicRadarChart.js
+++ dynamicRadarChart.js
@@ -11,6 +11,7 @@ function DynamicRadarChart(id, data, options) {
 	 h: 600,				//Height of the circle
 	 margin: {top: 30, right: 30, bottom: 30, left: 30}, //The margins around the circle
 	 legendPosition: {x: 5, y: 5}, // the position of the legend, from the top-left corner of the svg
+	 //legendPosition_other: {x: 500, y: 5}, // the position of the legend, from the top-left corner of the svg
 	 levels: 3,				//How many levels or inner circles should there be drawn
 	 maxValue: 0, 				//What is the value that the biggest circle will represent
 	 labelFactor: 1.175, 			//How much farther than the radius of the outer circle should the labels be placed
@@ -179,15 +180,15 @@ function DynamicRadarChart(id, data, options) {
 
 	//Text indicating at what % each level is
 	axisGrid.selectAll(".axisLabel")
-	   .data(d3.range(1,(cfg.levels+1)).reverse())
-	   .enter().append("text")
-	   .attr("class", "axisLabel")
-	   .attr("x", 4)
-	   .attr("y", function(d){return -d*radius/cfg.levels;})
-	   .attr("dy", "0.4em")
-	   .style("font-size", "10px")
-	   .attr("fill", "#737373")
-	   .text(function(d,i) { return Format(maxValue * d/cfg.levels); });
+		.data(d3.range(1,(cfg.levels+1)).reverse())
+		.enter().append("text")
+		.attr("class", "axisLabel")
+		.attr("x", 4)
+		.attr("y", function(d){return -d*radius/cfg.levels;})
+		.attr("dy", "0.4em")
+		.style("font-size", "10px")
+		.attr("fill", "#737373")
+		.text(function(d,i) { return Format(maxValue * d/cfg.levels); });
 
 	/////////////////////////////////////////////////////////
 	//////////////////// Draw the axes //////////////////////
@@ -404,5 +405,27 @@ function DynamicRadarChart(id, data, options) {
 
 svg.select(".legendOrdinal")
   .call(legendOrdinal);
+	
+	
+	
+	//	svg.append("g")
+  //	.attr("class", "legendOrdinal_other")
+  //	.attr("transform", "translate(" + cfg["legendPosition_other"]["x"] + "," + cfg["legendPosition_other"]["y"] + ")");
+
+//	var legendOrdinal_other = d3.legend.color()
+  //d3 symbol creates a path-string, for example
+  //"M0,-8.059274488676564L9.306048591020996,
+  //8.059274488676564 -9.306048591020996,8.059274488676564Z"
+  //	.shape("path", d3.svg.symbol().type("triangle-up").size(150)())
+  //	.shapePadding(10)
+  //	.scale(cfg.color)
+  //	.labels(cfg.color.domain().map(function(d){
+  //		return data[d][0][areaName];
+  //	}))
+  	//.on("cellover", function(d){ cellover(d); })
+  	//.on("cellout", function(d) { cellout(); });
+
+//svg.select(".legendOrdinal_other")
+  //.call(legendOrdinal_other);
 	
 }//RadarChart
